9316ab9151e6d3fe85830b76c3296d14129008b0
gnaneswari963 <gnaneswari22092000@gmail.com>
2023-11-23 14:03:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def krishna1(request):
+    return render(request,'krishna1.html')
+
+def krishna2(request):
+    return render(request,'krishna2.html')
+
+def krishna3(request):
+    return render(request,'krishna3.html')
+
+def flower1(request):
+    return render(request,'flower1.html')
+
+def flower2(request):
+    return render(request,'flower2.html')
+
+def flower3(request):
+    return render(request,'flower3.html')
